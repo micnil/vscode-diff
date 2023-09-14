129f7fcdb62150940b22c0fe775cd800847da754
Sergei Dyshel <qyron.private@gmail.com>
2023-09-14 15:42:58 +0300

    Add unimported
    Use `unimported` package to discover redundant files. Can be handy to clean
    not needed files after code udpate.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-export * from 'vs/editor/common/diff/linesDiffComputer';
+export * from './vs/editor/common/diff/linesDiffComputer';
 export * from './vs/base/common/diff/diff';
 export * from './vs/base/common/diff/diffChange';
 export * from './vs/editor/common/diff/legacyLinesDiffComputer';
 export * from './vs/editor/common/diff/advancedLinesDiffComputer';
-export * from './vs/editor/common/diff/linesDiffComputer';
