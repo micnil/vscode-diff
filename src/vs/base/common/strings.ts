63ee282c024250fcd5d999abf8b1e91d824bf7bc
Michael Nilsson <michael@nilsson.dev>
2025-11-04 21:34:31 +0100

    Remove unused function

--- src/vs/base/common/strings.ts
+++ src/vs/base/common/strings.ts
@@ -101,20 +101,6 @@ export function computeCodePoint(highSurrogate: number, lowSurrogate: number): n
 	return ((highSurrogate - 0xD800) << 10) + (lowSurrogate - 0xDC00) + 0x10000;
 }
 
-/**
- * get the code point that begins at offset `offset`
- */
-export function getNextCodePoint(str: string, len: number, offset: number): number {
-	const charCode = str.charCodeAt(offset);
-	if (isHighSurrogate(charCode) && offset + 1 < len) {
-		const nextCharCode = str.charCodeAt(offset + 1);
-		if (isLowSurrogate(nextCharCode)) {
-			return computeCodePoint(charCode, nextCharCode);
-		}
-	}
-	return charCode;
-}
-
 const enum CodePoint {
 	zwj = 0x200D,
 
